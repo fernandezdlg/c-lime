dd189b47f5feffad35721c7c55a7760b8f6e0829
fernandezdlg <juan.fernandezdelagarza@unibe.ch>
2025-02-12 16:02:24 +0200

    corrected the compilation script (needs cleaning)

--- compile.sh
+++ compile.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-source ../../load_modules.sh
+# source ../../load_modules.sh
 
 # build_dir="./build-$(date +%Y-%m-%d-%H:%M)" # FIXME: using this build path makes it fail...
-build_dir="./build"
+# build_dir="./build"
 
-autoconf configure.ac
+# autoconf configure.ac
 
-mkdir -p ${build_dir}
-cd ${build_dir}
-cmake ..
-make
+# mkdir -p ${build_dir}
+# cd ${build_dir}
+# make
 
+export CC=mpicc
+
+aclocal
+autoconf
+autoheader
+automake --add-missing
+./configure --prefix=/users/juananto/project_465000412/juananto/software/c-lime/build
+make install
